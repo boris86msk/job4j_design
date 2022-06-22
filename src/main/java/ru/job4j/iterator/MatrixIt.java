2ae28f305cb77f2fd22083346d39b731c06528e8
User <boris86msk@yandex.ru>
2022-06-22 20:12:28 +0300

    version 2

--- src/main/java/ru/job4j/iterator/MatrixIt.java
+++ src/main/java/ru/job4j/iterator/MatrixIt.java
@@ -14,40 +14,17 @@ public class MatrixIt implements Iterator<Integer> {
 
     @Override
     public boolean hasNext() {
-        boolean rst = true;
-        while (rst) {
-            if (data.length > 1) {
-                if (data[row].length == 0 && row != data.length - 1) {
-                    row++;
-                } else if (row == data.length - 1) {
-                    if (data[row].length == 0) {
-                        rst = false;
-                    } else {
-                        break;
-                    }
-                } else {
-                    break;
-                }
-            } else if (data[row].length == 0) {
-                rst = false;
-            } else {
-                break;
-            }
+        while (column < data[row].length || data[row].length == 0) {
+            row++;
+            column = 0;
         }
-        return rst;
+        return row < data.length && column < data[row].length;
     }
 
     @Override
     public Integer next() {
         if (!hasNext()) {
             throw new NoSuchElementException();
-        }
-        if (column >= data[row].length) {
-            row++;
-            column = 0;
-            while (data[row].length == 0) {
-                row++;
-            }
         }
         return data[row][column++];
     }
