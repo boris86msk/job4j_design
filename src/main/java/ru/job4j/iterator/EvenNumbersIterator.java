9bc67678706fca8c09c090dd7c1c264cb34bad22
boris86msk <boris86msk@yandex.ru>
2022-06-23 20:53:56 +0300

    Итератор для четных 3

--- src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
+++ src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -2,6 +2,7 @@ package ru.job4j.iterator;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+
 public class EvenNumbersIterator implements Iterator<Integer> {
     private int[] data;
     private int index;
