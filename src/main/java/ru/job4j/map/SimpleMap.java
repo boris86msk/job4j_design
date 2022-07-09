8dda55446661fd03a204ae559d5c651c0448b98a
User <boris86msk@yandex.ru>
2022-07-09 20:17:01 +0300

    Реализовать HashMap [#1008]

---
+++ src/main/java/ru/job4j/map/SimpleMap.java
@@ -0,0 +1,132 @@
+package ru.job4j.map;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleMap<K, V> implements Map<K, V> {
+
+    private static final float LOAD_FACTOR = 0.75f;
+
+    private int capacity = 8;
+
+    private int count = 0;
+
+    private int modCount = 0;
+
+    private MapEntry<K, V>[] table = new MapEntry[capacity];
+
+    @Override
+    public boolean put(K key, V value) {
+        if ((float) count / capacity >= LOAD_FACTOR) {
+            expand();
+        }
+        MapEntry<K, V> newCouple = new MapEntry<>(key, value);
+        int index = indexFor(hash(key));
+        if (table[index] != null) {
+            return false;
+        }
+        table[index] = newCouple;
+        capacity++;
+        count++;
+        return true;
+    }
+
+    private int hash(K key) {
+        int h;
+        return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
+    }
+
+    private int indexFor(int hash) {
+        return hash & (table.length - 1);
+    }
+
+    private void expand() {
+        capacity *= 2;
+        MapEntry<K, V>[] newTable = new MapEntry[capacity];
+        for (int i = 0; i < table.length; i++) {
+            if (table[i] != null) {
+                MapEntry<K, V> newCouple = table[i];
+                int index = indexFor(hash(newCouple.key));
+                newTable[index] = newCouple;
+            }
+        }
+        table = newTable;
+    }
+
+    @Override
+    public V get(K key) {
+        V rst = null;
+        for (int i = 0; i < table.length; i++) {
+            if (table[i] != null && table[i].key.equals(key)) {
+                rst = table[i].value;
+                break;
+            }
+        }
+        return rst;
+    }
+
+    @Override
+    public boolean remove(K key) {
+        boolean rst = false;
+        for (int i = 0; i < table.length; i++) {
+            if (table[i] != null && table[i].key.equals(key)) {
+                table[i] = null;
+                rst = true;
+                capacity++;
+                count--;
+                break;
+            }
+        }
+        return rst;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return new Iterator<>() {
+            private final int modCountEntrance = modCount;
+            private int index = 0;
+            final MapEntry<K, V>[] iteratorTable = overwriting();
+
+
+            @Override
+            public boolean hasNext() {
+                if (modCountEntrance != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return index < iteratorTable.length;
+            }
+
+            @Override
+            public K next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (K) iteratorTable[index++];
+            }
+
+            private MapEntry<K, V>[] overwriting() {
+                MapEntry<K, V>[] iteratorTable = new MapEntry[count];
+                int index = 0;
+                for (int i = 0; i < table.length; i++) {
+                    if (table[i] != null) {
+                        iteratorTable[index++] = table[i];
+                    }
+                }
+                return iteratorTable;
+            }
+        };
+    }
+
+    private static class MapEntry<K, V> {
+
+        K key;
+        V value;
+
+        public MapEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+}
---
+++ src/test/java/ru/job4j/map/SimpleMapTest.java
@@ -0,0 +1,78 @@
+package ru.job4j.map;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Iterator;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class SimpleMapTest {
+
+    @Test
+    public void WhenPutOneElement() {
+        SimpleMap<User, String> map = new SimpleMap<>();
+        Calendar calendar = new GregorianCalendar();
+        calendar.set(1986, 2, 15);
+        User user = new User("Bob", 2, calendar);
+        assertTrue(map.put(user, "10000"));
+        assertFalse(map.put(user, "10000"));
+    }
+
+    @Test
+    public void WenPutTwoElements() {
+        SimpleMap<User, String> map = new SimpleMap<>();
+        Calendar calendar = new GregorianCalendar();
+        calendar.set(1986, 2, 15);
+        User user1 = new User("Bob", 2, calendar);
+        User user2 = new User("Ivan", 1, calendar);
+        assertNotEquals(user1.hashCode(), user2.hashCode());
+        map.put(user1, "400");
+        assertTrue(map.put(user2, "500"));
+    }
+
+    @Test
+    public void Iterator() {
+        SimpleMap<User, String> map = new SimpleMap<>();
+        Calendar calendar = new GregorianCalendar();
+        calendar.set(1986, 2, 15);
+        User user1 = new User("Bob", 2, calendar);
+        User user2 = new User("Ivan", 1, calendar);
+        map.put(user1, "200");
+        map.put(user2, "350");
+        Iterator<User> iterator = map.iterator();
+        iterator.next();
+        assertTrue(iterator.hasNext());
+        iterator.next();
+        assertFalse(iterator.hasNext());
+    }
+
+    @Test
+    public void WenGetТotExistingKey() {
+        SimpleMap<User, String> map = new SimpleMap<>();
+        Calendar calendar = new GregorianCalendar();
+        calendar.set(1986, 2, 15);
+        User user1 = new User("Bob", 2, calendar);
+        User user2 = new User("Ivan", 1, calendar);
+        User user3 = new User("Jon", 0, calendar);
+        map.put(user1, "200");
+        map.put(user2, "350");
+        assertThat(map.get(user2), is("350"));
+        assertNull(map.get(user3));
+    }
+
+    @Test
+    public void WhenRemoveOneElement() {
+        SimpleMap<User, String> map = new SimpleMap<>();
+        Calendar calendar = new GregorianCalendar();
+        calendar.set(1986, 2, 15);
+        User user = new User("Bob", 2, calendar);
+        User user2 = new User("Jon", 0, calendar);
+        map.put(user2, "0");
+        assertTrue(map.put(user, "10000"));
+        assertTrue(map.remove(user));
+        assertNull(map.get(user));
+    }
+}
