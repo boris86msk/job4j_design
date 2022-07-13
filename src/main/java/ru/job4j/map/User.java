bf77346226c66ac4a35d68a730b05c87edef28c5
boris86msk <boris86msk@yandex.ru>
2022-07-13 09:44:06 +0300

    0.1

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,10 +1,8 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
-import java.util.Objects;
 
 public class User {
-
     private String name;
     private int children;
     private Calendar birthday;
@@ -13,31 +11,5 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        User user = (User) o;
-        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, children, birthday);
-    }
-
-    @Override
-    public String toString() {
-        return "User{"
-                + "name='" + name + '\''
-                + ", children=" + children
-                + ", birthday=" + birthday
-                + '}';
     }
 }
