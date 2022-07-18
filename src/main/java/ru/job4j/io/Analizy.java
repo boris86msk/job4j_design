ced4d3547df08810252735633c5bfc361755f2cf
boris86msk <boris86msk@yandex.ru>
2022-07-18 21:48:00 +0300

    Анализ доступности сервера 2

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,28 +1,23 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-        List<String> list;
         try (BufferedReader in = new BufferedReader(new FileReader(source));
              PrintWriter out = new PrintWriter(
                      new BufferedOutputStream(
                              new FileOutputStream(target)))) {
-            list = in.lines()
-                    .filter(e -> !e.isEmpty())
-                    .toList();
-
             boolean server = true;
             String timeStart = "";
-            for (String line : list) {
-                if (server && (line.startsWith("400") || line.startsWith("500"))) {
-                    timeStart = line.split(" ")[1];
+            String read;
+            while ((read = in.readLine()) != null) {
+                if (server && (read.startsWith("400") || read.startsWith("500"))) {
+                    timeStart = read.split(" ")[1];
                     server = false;
-                } else if (!server && (line.startsWith("200") || line.startsWith("300"))) {
-                    out.println(timeStart + ";" + line.split(" ")[1] + ";");
+                } else if (!server && (read.startsWith("200") || read.startsWith("300"))) {
+                    out.println(timeStart + ";" + read.split(" ")[1] + ";");
                     server = true;
                 }
             }
