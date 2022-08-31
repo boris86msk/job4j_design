fbf74c3f522dadad914f32a9e1da81d72eebf7e3
boris86msk <boris86msk@yandex.ru>
2022-08-31 22:31:00 +0300

    Поиск файлов по критерию [#783]

---
+++ src/main/java/ru/job4j/io/filesearcher/ArgsName.java
@@ -0,0 +1,64 @@
+package ru.job4j.io.filesearcher;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        if (!values.containsKey(key)) {
+            throw new IllegalArgumentException(
+                    "no data available");
+        }
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        values.putAll(Arrays.stream(args)
+                .filter(e -> {
+                    argsValid(e);
+                    return true;
+                })
+                .map(e -> e.substring(1))
+                .map(e -> e.split("=", 2))
+                .collect(Collectors.toMap(e -> e[0], e -> e[1])));
+    }
+
+
+    public static ArgsName of(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException(
+                    "no data available");
+        }
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    private void argsValid(String arg) {
+        if (!arg.startsWith("-")) {
+            throw new IllegalArgumentException(
+                    "the string does not start with the character \"-\"");
+        }
+        if (!arg.contains("=")) {
+            throw new IllegalArgumentException(
+                    "the equal sign is missing");
+        }
+        if (arg.startsWith("-=") || arg.indexOf("=") == arg.length() - 1) {
+            throw new IllegalArgumentException(
+                    "there is no key or value in the parameter");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "ArgsName{"
+                + "values=" + values
+                + '}';
+    }
+}
+
---
+++ src/main/java/ru/job4j/io/filesearcher/Searcher.java
@@ -0,0 +1,123 @@
+package ru.job4j.io.filesearcher;
+
+import ru.job4j.io.SearchFiles;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
+public class Searcher {
+    private static Path path;
+    private  static String searchObject;
+    private static String saveFile;
+
+    private Searcher(Path path, String searchObject, String saveFile) {
+        Searcher.path = path;
+        Searcher.searchObject = searchObject;
+        Searcher.saveFile = saveFile;
+    }
+    /**
+     * Ключи
+     * -d - директория, в которой начинать поиск.
+     * -n - имя файла, маска, либо регулярное выражение.
+     * -t - тип поиска: mask искать по маске, name по полному совпадение имени, regex по регулярному выражению.
+     * -o - результат записать в файл.
+     * @param args
+     */
+
+    public static void main(String[] args) throws IOException {
+        ArgsName arg = ArgsName.of(args);
+        Path path = Paths.get(arg.get("d"));
+        String searchObject = arg.get("n");
+        String saveFile = arg.get("o");
+        Searcher searcher = new Searcher(path, searchObject, saveFile);
+
+        paramValidate(arg);
+        switch (arg.get("t")) {
+            case ("mask"):
+                scanByMask();
+                break;
+            case ("name"):
+                searchByName();
+                break;
+            case ("regex"):
+                searchByRegex();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private static void scanByMask() throws IOException {
+        StringBuilder regex = new StringBuilder();
+        for (int i = 0; i < searchObject.length(); i++) {
+            if (searchObject.charAt(i) == '*') {
+                regex.append("^.+");
+            } else if (searchObject.charAt(i) == '?') {
+                regex.append("^.");
+            } else {
+                regex.append(searchObject.charAt(i));
+            }
+        }
+        searchObject = regex.toString();
+        searchByRegex();
+    }
+
+    private static void searchByName() throws IOException {
+        Predicate<Path> pred = p -> p.toFile().getName().endsWith(searchObject);
+        List<Path> list = search(path, pred);
+        save(list, saveFile);
+    }
+
+    private static void searchByRegex() throws IOException {
+        Predicate<Path> pred = p -> Pattern.matches(searchObject, p.toFile().getName());
+        List<Path> list = search(path, pred);
+        save(list, saveFile);
+    }
+
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        SearchFiles searcher = new SearchFiles(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+
+    public static void save(List<Path> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)
+                ))) {
+            for (Path path : log) {
+                out.printf("%s%n", path);
+            }
+        } catch (Exception ioexception) {
+            ioexception.printStackTrace();
+        }
+    }
+
+    static void paramValidate(ArgsName args) {
+        if (args.get("d") == null || args.get("n") == null
+                || args.get("t") == null || args.get("o") == null) {
+            throw new IllegalArgumentException("insufficient number of arguments.");
+        }
+        File file = new File(args.get("d"));
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format(
+                    "Not exist %s", file.getAbsolutePath()
+            ));
+        } else if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format(
+                    "Not directory %s", file.getAbsoluteFile()
+            ));
+        }
+        if (!(args.get("t").equals("mask") || args.get("t").equals("name")
+                || args.get("t").equals("regex"))) {
+            throw new IllegalArgumentException(String.format(
+                    "invalid search type parameter \"%s\" ", args.get("t")
+            ));
+        }
+    }
+}
