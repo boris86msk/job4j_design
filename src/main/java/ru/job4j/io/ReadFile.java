bbf78b7c01d0d0beba4c4f91e7dcdf24b6e325d3
boris86msk <boris86msk@yandex.ru>
2022-09-02 23:23:50 +0300

    checkstyle err

--- src/main/java/ru/job4j/io/ReadFile.java
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -18,26 +18,23 @@ public class ReadFile {
                 text.append((char) read);
                 System.out.println(text);
             }
-
-
-
-//            String[] lines = text.toString().split(System.lineSeparator());
-//            for (String line : lines) {
-//                System.out.println(line);
-//            }
+            String[] lines = text.toString().split(System.lineSeparator());
+            for (String line : lines) {
+                System.out.println(line);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-//        try (FileReader rid = new FileReader("input.txt")) {
-//            StringBuilder text = new StringBuilder();
-//            int rd;
-//            while ((rd = rid.read()) != -1) {
-//                text.append((char) rd);
-//                System.out.println(text);
-//            }
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        try (FileReader rid = new FileReader("input.txt")) {
+            StringBuilder text = new StringBuilder();
+            int rd;
+            while ((rd = rid.read()) != -1) {
+                text.append((char) rd);
+                System.out.println(text);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
