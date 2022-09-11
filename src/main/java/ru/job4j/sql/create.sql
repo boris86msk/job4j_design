1c1831f6eb528f69c5a179d6e60a2b31a1648f0f
boris86msk <boris86msk@yandex.ru>
2022-09-11 15:52:35 +0300

    Схема прав пользователи и роли. [#1093 #341919]

--- src/main/java/ru/job4j/sql/create.sql
+++ src/main/java/ru/job4j/sql/create.sql
@@ -1,21 +1,21 @@
 create table role (
     id serial primary key,
-    name varchar(255),
+    name varchar(255)
 );
 
 create table category (
     id serial primary key,
-    name varchar(255),
+    name varchar(255)
 );
 
 create table state (
     id serial primary key,
-    name varchar(255),
+    name varchar(255)
 );
 
 create table rules (
     id serial primary key,
-    name varchar(255),
+    name varchar(255)
 );
 
 create table users (
@@ -27,7 +27,7 @@ create table users (
 create table item (
     id serial primary key,
     number int,
-    user int references users(id),
+    users int references users(id),
     category int references category(id),
     state int references state(id)
 );
@@ -47,5 +47,5 @@ create table attachs (
 create table rules_for_role (
     id serial primary key,
     role_id int references role(id),
-    rules_id int reference rules(id)
+    rules_id int references rules(id)
 );
