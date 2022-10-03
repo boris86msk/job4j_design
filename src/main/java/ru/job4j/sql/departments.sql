bca851ab2004b2aef4084f3446e8438c8e23d275
User <boris86msk@yandex.ru>
2022-10-03 21:36:30 +0300

    0. Join [#6862]

--- src/main/java/ru/job4j/sql/departments.sql
+++ src/main/java/ru/job4j/sql/departments.sql
@@ -26,3 +26,33 @@ insert into employees(name, dep_id) values('Jennifer Clarke', 1);
 insert into employees(name, dep_id) values('Pauline Allen', 3);
 insert into employees(name, dep_id) values('Alan Jones', 5);
 insert into employees(name, dep_id) values('Linda Morris', 2);
+insert into employees(name, dep_id) values('Kelly Salevan', null);
+
+SELECT d.id, d.name, e.name
+FROM departments d
+LEFT JOIN employees e ON e.dep_id = d.id;
+
+SELECT d.id, d.name, e.name
+FROM departments d
+RIGHT JOIN employees e ON e.dep_id = d.id;
+
+SELECT d.id, d.name, e.name
+FROM departments d
+FULL JOIN employees e ON e.dep_id = d.id;
+
+SELECT d.id, d.name, e.name
+FROM departments d
+CROSS JOIN employees e;
+
+SELECT d.id, d.name
+from departments d
+LEFT JOIN employees e ON e.dep_id = d.id
+WHERE e.dep_id IS NULL
+
+SELECT d.id, d.name, e.name
+from departments d
+LEFT JOIN employees e ON e.dep_id = d.id
+
+SELECT d.id, d.name, e.name
+from employees e
+RIGHT JOIN departments d ON e.dep_id = d.id
---
+++ src/main/java/ru/job4j/sql/gender_table.sql
@@ -0,0 +1,24 @@
+CREATE TABLE teens (
+	id serial primary key,
+	name varchar,
+	gender varchar(4)
+);
+
+insert into teens(name, gender) values('Анна', 'woman');
+insert into teens(name, gender) values('Ирина', 'woman');
+insert into teens(name, gender) values('Ольга', 'woman');
+insert into teens(name, gender) values('Татьяна', 'woman');
+insert into teens(name, gender) values('Генадий', 'man');
+insert into teens(name, gender) values('Артем', 'man');
+insert into teens(name, gender) values('Василий', 'man');
+insert into teens(name, gender) values('Виталий', 'man');
+
+SELECT CONCAT(t1.name, ' + ', t2.name)
+FROM teens t1
+CROSS JOIN teens t2
+WHERE t1.gender != t2.gender
+LIMIT ((SELECT COUNT(*)
+FROM teens
+WHERE gender LIKE 'man') * (SELECT COUNT(*)
+FROM teens
+WHERE gender LIKE 'woman'))
