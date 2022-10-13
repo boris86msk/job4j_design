f92102014ae14d0e3a7e534178b4d1f66e5b7f03
boris86msk <boris86msk@yandex.ru>
2022-10-13 22:48:04 +0300

    5. Курсор в SQL [#504816]

---
+++ src/main/java/ru/job4j/sql/cursor.sql
@@ -0,0 +1,12 @@
+--транзакция с курсором для таблицы "products" из задания
+--    "5. Курсор в SQL [#504816]"
+begin transaction;
+DECLARE curs1 SCROLL CURSOR FOR SELECT * FROM products;
+FETCH BACKWARD 4 FROM curs1;
+FETCH BACKWARD FROM curs1;
+MOVE BACKWARD FROM curs1;
+MOVE BACKWARD 5  FROM curs1;
+FETCH BACKWARD FROM curs1;
+FETCH FIRST FROM curs1;
+CLOSE curs1;
+COMMIT;
