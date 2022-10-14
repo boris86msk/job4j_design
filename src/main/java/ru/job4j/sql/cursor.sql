032700ae6363068f073111fc776ea787be2465c4
User <boris86msk@yandex.ru>
2022-10-14 16:22:53 +0300

    5. Курсор в SQL 2

--- src/main/java/ru/job4j/sql/cursor.sql
+++ src/main/java/ru/job4j/sql/cursor.sql
@@ -2,6 +2,7 @@
 --    "5. Курсор в SQL [#504816]"
 begin transaction;
 DECLARE curs1 SCROLL CURSOR FOR SELECT * FROM products;
+FETCH LAST FROM curs1;
 FETCH BACKWARD 4 FROM curs1;
 FETCH BACKWARD FROM curs1;
 MOVE BACKWARD FROM curs1;
